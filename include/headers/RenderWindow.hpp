26a8349d2c53e12c6feb7d59d9b8547cf0155b51
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2022-04-16 03:38:19 -0400

    Add files via upload

---
+++ include/headers/RenderWindow.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+
+class RenderWindow
+{
+    public:
+        RenderWindow(const char* pTitle, int pWidth, int pHeight);
+        void cleanUp();
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+};
